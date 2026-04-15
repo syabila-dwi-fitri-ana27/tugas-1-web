02e159ea2ae34303a686e727b0ae72d06e603fc7
syabila-dwi-fitri-ana27 <syabiladwifitriana34@gmail.com>
2026-04-15 12:13:20 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="in">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 1 Pemrograman Web</title>
+</head>
+<body>
+    <div style="border: 2px solid black ; padding: 10px; display: flex; align-items: center;">
+    <img src="Logo Universitas Sains Al-Qur'an (UNSIQ) [RiderGalau].png" alt="logo unsiq"  width="200">
+    <div style="margin-left: 15px;">
+        <h1 style="margin: 0;">Selamat Datang di Mata Kuliah Pemrograman Web</h1>
+        <h2>Pertemuan 2: HTML Dasar</h2>
+    </div>
+    </div>
+
+    <div style="text-align: center;">
+       <p style="margin: 10;">Mata kuliah ini memperkenalkan dasar-dasar pemrograman web menggunakan HTML, CSS, dan JavaScript
+       </p>
+       <p style="border-bottom: 1px solid black; padding-bottom: 20px;">
+       Pada pertemuan ini, kita fokus pada struktur da elemen dasar HTML.
+       </p> 
+    </div>
+
+    <p>HTML adalah bahasa markup yang digunakan untuk membuat struktur halaman web, dengan HTML,
+         kita bisa menampilkan teks, gambar, dan link secara terorganisir. Untuk informasi lengkap dari HTML
+        dapat kunjungi link beikut:
+        <a href="https://if.unpas.ac.id/berita/apa-itu-html-pengertian-sejarah-dan-bagaimana-cara-kerjanya/">Penjelasan HTML</a>
+    </p>
+
+    <p><b>Topik Utama Pertemuan Ini:</b></p>
+    <ul>
+        <li>Struktur HTML</li>
+        <li>Heading dan Paragraf</li>
+        <li>List (Unordered dan Ordered)</li>
+        <li>Link dan Gambar</li>
+        <li>Tabel</li>
+    </ul>
+
+    <p><b>Langkah Praktikum:</b></p>
+    <ol>
+        <li>Buat file index php</li>
+        <li>Tulis kode HTML</li>
+        <li>Simpan dan buka di browser</li>
+        
+           <ul>
+              <li>buka xampp</li>
+              <li>nyalakan server</li>
+              <li>buka browser dengan url localhost/...</li>
+           </ul>
+        <li>Verivikasi Hasil</li>
+    </ol>
+
+     
+    
+
+</body>
+</html>
